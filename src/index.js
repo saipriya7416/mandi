7f24c3a45c028d37293f7fcf1c4f354b39f823e9
saipriya <priyasaravana7416@gmail.com>
2026-03-18 07:59:50 +0530

    premium upgrade

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-// index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // Tailwind CSS
+import "./index.css"; // Tailwind styles
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
